c9f5e457eeb260e39a2a539b6dbd97dbc442914d
Masci, Richard (rx7322) <rx7322@us.att.com>
2024-01-23 11:46:07 -0500

    Added new example in documentation that works.

--- example/main.go
+++ example/main.go
@@ -1,47 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/rmasci/fileserver"
 )
 
 var lgOut *log.Logger
-var nginx string
 
 func main() {
-	lgOut = log.New(os.Stdout, "", log.Lshortfile)
-	nginx = "nginx"
-	lgOut.Println(pattern("downloads"))
+	html := os.Getenv("PWD") + "/html"
 	dwnld := fileserver.Directory{
-		Px:      15,
-		Srv:     "/var/www/html",
-		BaseURI: pattern("downloads"),
-		Lgout:   lgOut,
-		Header:  "MyFiles",
+		Lgout:  log.New(os.Stdout, "", log.Lshortfile),
+		Px:     15,
+		Srv:    html,
+		Header: "MyFiles",
 	}
 
-	// Start web server
-	http.Handle(pattern("downloads"), http.StripPrefix(pattern("downloads/"), http.HandlerFunc(dwnld.Fileserver)))
-	http.HandleFunc("/", printpath)
-	err := http.ListenAndServe(":8789", nil)
-	if err != nil {
-		fmt.Println("Error")
-	}
-}
-func pattern(path string) string {
-	path = strings.Trim(path, "/")
-	nginx = strings.Trim(nginx, "/")
-	retstr := fmt.Sprintf("/%s/%s/", nginx, path)
-	retstr = strings.ReplaceAll(retstr, "//", "/")
-	return retstr
-}
-
-func printpath(w http.ResponseWriter, r *http.Request) {
-	lgOut.Println(r.RequestURI)
-	fmt.Fprintf(w, "<html><h1>URI</h1><hr>%v</html>", r.RequestURI)
+	http.HandleFunc("/downloads/", dwnld.Fileserver)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
---
+++ example/one/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/rmasci/fileserver"
+)
+
+var lgOut *log.Logger
+var nginx string
+
+func main() {
+	lgOut = log.New(os.Stdout, "", log.Lshortfile)
+	nginx = "nginx"
+	lgOut.Println(pattern("downloads"))
+	dwnld := fileserver.Directory{
+		Px:      15,
+		Srv:     "html/",
+		BaseURI: pattern("downloads"),
+		Lgout:   lgOut,
+		Header:  "MyFiles",
+	}
+
+	// Start web server
+	//http.Handle(pattern("/html/"), http.StripPrefix(pattern("/html/"), http.HandlerFunc(dwnld.Fileserver)))
+	//http.HandleFunc("/", printpath)
+	err := http.ListenAndServe(":8789", http.HandlerFunc(dwnld.Fileserver))
+	if err != nil {
+		fmt.Println("Error")
+	}
+}
+
+func pattern(path string) string {
+	path = strings.Trim(path, "/")
+	nginx = strings.Trim(nginx, "/")
+	retstr := fmt.Sprintf("/%s/%s/", nginx, path)
+	retstr = strings.ReplaceAll(retstr, "//", "/")
+	return retstr
+}
+
+func printpath(w http.ResponseWriter, r *http.Request) {
+	lgOut.Println(r.RequestURI)
+	fmt.Fprintf(w, "<html><h1>URI</h1><hr>%v</html>", r.RequestURI)
+}
